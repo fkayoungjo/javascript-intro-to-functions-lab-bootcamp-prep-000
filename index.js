6c3a6ac42085fd8cb59ffba824700e5851ae8ae1
Jordan Simon <jordan.simon55@gmail.com>
2018-08-08 20:52:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function shout (string) {
 function whisper (string) {
   return string.toLowerCase()
 }
+function sayHiToGrandma (string) {
+  if string === string.lowercase()
+  console.log("I can't hear you!")
+}
